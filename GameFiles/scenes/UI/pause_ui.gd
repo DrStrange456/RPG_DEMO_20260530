ccfc1b282500033732e769d91c52b670c837f917
DrStrange456 <75033117+DrStrange456@users.noreply.github.com>
2026-06-14 19:29:43 -0400

    jkjl

--- GameFiles/scenes/UI/pause_ui.gd
+++ GameFiles/scenes/UI/pause_ui.gd
@@ -1,1 +1,27 @@
 extends popup_ui
+
+
+
+func _on_btn_new_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_save_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_load_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_quit_pressed() -> void:
+	get_tree().quit()
+
+
+
+
+# Bottom
